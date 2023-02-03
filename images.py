0cca10da5da72da66ca4ecfed3f2b02e4e61c7fd
wdkaoidawjuod7u93y48 <124058628+wdkaoidawjuod7u93y48@users.noreply.github.com>
2023-02-03 11:15:58 +0300

    Update images.py

--- images.py
+++ images.py
@@ -27,7 +27,45 @@ async def index(request):
   return redirect("")
 
 
+@app.route("/images/playlists/{id}/showcase.png")
+async def images(request, id):
+  try:
+    id = id.upper()
+    if path.exists(f"images/{id}"):
+      return await file(f"images/{id}.png")
+    else:
+      image_url = f"https://fortnite-api.com/images/playlists/{id}/showcase.png"
+      with urllib.request.urlopen(image_url) as url:
+          image_data = url.read()
+      with open("temp.png", "wb") as f:
+          f.write(image_data)
+      image = Image.open("temp.png")
+      draw = ImageDraw.Draw(image)
+      font = ImageFont.truetype("Provicali.otf", 62)
+      text_width, text_height = draw.textsize("KllDFN", font=font)
+      x = (image.width - text_width) // 2
+      y = (image.height - text_height) // 2
+      try:
+        draw.text((x, y), "KllDFN", font=font, fill=(256, 64))
+      except:
+        draw.text((x, y), "KllDFN", font=font, fill=(0, 0, 0, 64))
+      image.save(f"images/{id}.png")
+      return await file(f"images/{id}.png")
+  except Exception as e:
+    return text(str(e))
 
+async def statusLoop():
+  while True:
+    images = listdir("images")
+    game = discord.Activity(
+      type=discord.ActivityType.watching, 
+      name=f"{len(images)} Images"
+    )
+    await bot.change_presence(
+      status=discord.Status.dnd, 
+      activity=game
+    )
+    await sleep(300)
 
 
 @app.route("/images/<id>/icon.png")
@@ -94,6 +132,19 @@ async def gen(ctx: discord.ApplicationContext, id:str):
   embed.set_author(name="Image Generated", url=f"{url}/images/{id}/icon.png", icon_url=f"{url}/images/{id}/icon.png")
   embed.set_thumbnail(url=f"{url}/images/{id}/icon.png")
   embed.set_image(url=f"{url}/images/{id}/icon.png")
+  await ctx.respond(embed=embed)
+  
+  @bot.slash_command(name="playlists")
+@option(
+    "id",
+    description="An ID For a Valid Fortnite Cosmetic",
+    required=True
+)
+async def gen(ctx: discord.ApplicationContext, id:str):
+  embed=discord.Embed(title=f"{id}", url=f"{url}/images/playlists/{id}/showcase.png", description=f"{id} Image Generated")
+  embed.set_author(name="Image Generated", url=f"{url}/images/playlists/{id}/showcase.png", icon_url=f"{url}/images/playlists/{id}/showcase.png")
+  embed.set_thumbnail(url=f"{url}/images/playlists/{id}/showcase.png")
+  embed.set_image(url=f"{url}/images/playlists/{id}/showcase.png")
   await ctx.respond(embed=embed)
 
 
