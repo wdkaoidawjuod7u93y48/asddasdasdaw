ded86b00a79a408fc9d7f215471ad5b07636f2a2
wdkaoidawjuod7u93y48 <124058628+wdkaoidawjuod7u93y48@users.noreply.github.com>
2023-03-05 17:34:26 +0300

    Update authcode.py

--- authcode.py
+++ authcode.py
@@ -934,10 +934,14 @@ SECRET="{auths['secret']}"</textarea><br>''' + '''
 <h1 class="hero-title mt-0"></h1><br>
 <br><br>
 <textarea rows=3 style=color:#fff;resize:none;width:95%;font-size:15px;background-color:#333;color:white;border:0;border-radius:20px id=deviceAuthText class="form-control drop" data-cf-modified-f01948e756116e48c69a07b1->'''
+<textarea rows="3" style="resize: none; width: 100%;" id="deviceAuthsText" class="form-control drop">
 + f'''
 DEVICE_ID="{auths['deviceId']}"
 ACCOUNT_ID="{auths['accountId']}"
-SECRET="{auths['secret']}"</textarea><br>''' + '''
+SECRET="{auths['secret']}
+''' + '''
+</textarea>
+<br/>
 <br>
 <section class="features section">
 <div class=container>
