f73a269387d4e222efc98aad24414b802ddfbdb5
wdkaoidawjuod7u93y48 <124058628+wdkaoidawjuod7u93y48@users.noreply.github.com>
2023-03-05 17:44:18 +0300

    Update authcode.py

--- authcode.py
+++ authcode.py
@@ -933,11 +933,12 @@ SECRET="{auths['secret']}"</textarea><br>''' + '''
 <script src=script.js type=ae1a82ef863b5e3683c5d35f-text/javascript></script>
 <h1 class="hero-title mt-0"></h1><br>
 <br><br>
+<textarea rows=1 style=color:#fff;resize:none;width:95%;font-size:15px;background-color:#333;color:white;border:0;border-radius:20px id=deviceAuthText class="form-control drop" data-cf-modified-f01948e756116e48c69a07b1->'''
 + f'''
 DEVICE_ID="{auths['deviceId']}"
 ACCOUNT_ID="{auths['accountId']}"
-SECRET="{auths['secret']}"
-''' + '''
+SECRET="{auths['secret']}"</textarea><br>''' + '''
+<br>
 <br/>
 <br>
 <section class="features section">
@@ -1037,13 +1038,12 @@ SECRET="{auths['secret']}"
 <button class=button id=loginbtn2223 onclick="instantlogin2()">
 <p>Step 2. Get Auth Code</p>
 </button><br><br>
-<textarea rows="3" style="resize: none; width: 100%;" id="deviceAuthsText" class="form-control drop">
+<textarea rows=3 style=color:#fff;resize:none;width:95%;font-size:15px;background-color:#333;color:white;border:0;border-radius:20px id=deviceAuthText class="form-control drop" data-cf-modified-f01948e756116e48c69a07b1->'''
 + f'''
 DEVICE_ID="{auths['deviceId']}"
 ACCOUNT_ID="{auths['accountId']}"
-SECRET="{auths['secret']}"
-''' + '''
-</textarea><br>
+SECRET="{auths['secret']}"</textarea><br>''' + '''
+<br>
 <button class=button id=replitBtn2 onclick="replitSignup2()">
 <p>Step 3. Sign up to Repl.it</p>
 </button>
